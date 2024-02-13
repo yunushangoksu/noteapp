e2e20128489bf8505c7a6a61faf823678e34c92a
yunushangoksu <yunushangoksu@gmail.com>
2024-02-13 06:00:25 +0300

    added font awesome

--- client/src/modules/MainPage.jsx
+++ client/src/modules/MainPage.jsx
@@ -1,18 +1,26 @@
 import "../styles/MainPage.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faLightbulb, faBoxArchive, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 function MainPage() {
   return (
     <div>
       <div className="header">
         <div className="logoWrapper">
-          <div className="navbarButton">X</div>
+          <div className="navbarButton">
+            <FontAwesomeIcon icon={faBars} size="xl" />
+          </div>
 
           <div className="logo">Noteapp</div>
         </div>
         <input type="text" className="searchInput" placeholder="Search" />
         <div className="currentUser">Current user</div>
       </div>
-      <div className="navBar">Navbar</div>
+      <div className="navBar">
+        <FontAwesomeIcon icon={faLightbulb} size="xl" fixedWidth />
+        <FontAwesomeIcon icon={faBoxArchive} size="xl" fixedWidth />
+        <FontAwesomeIcon icon={faTrash} size="xl" fixedWidth />
+      </div>
     </div>
   );
 }
